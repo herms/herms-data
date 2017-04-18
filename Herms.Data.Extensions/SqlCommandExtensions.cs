6593ede2abc3ae4711ffe8b389182f4550902b93
Herman Crawfurd Svensen <h@herms.no>
2017-04-18 23:22:22 +0200

    Added source of AddArrayParameters method.

--- Herms.Data.Extensions/SqlCommandExtensions.cs
+++ Herms.Data.Extensions/SqlCommandExtensions.cs
@@ -6,6 +6,17 @@ namespace Herms.Data.Extensions
 {
     public static class SqlCommandExtensions
     {
+        /// <summary>
+        /// Found at: http://stackoverflow.com/a/2377651
+        /// Created by: http://stackoverflow.com/users/320/brian
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="cmd"></param>
+        /// <param name="paramNameRoot"></param>
+        /// <param name="values"></param>
+        /// <param name="start"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
         public static SqlParameter[] AddArrayParameters<T>(this SqlCommand cmd, string paramNameRoot,
             IEnumerable<T> values,
             int start = 1, string separator = ", ")
